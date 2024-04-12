387d3c419674e84f0245569745659c1c8de54e88
MeshachHeinrich <42801000+MeshachHeinrich@users.noreply.github.com>
2024-04-12 16:49:46 +0930

    Transferring files to different machine

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,27 @@
 // Generate data -------------------------------------------------
 function generateData() {
 
-    var data = []; 
+    var data = $('#data').data('values');
+    alert(data);
     var dataSize = parseInt(document.getElementById("datasize").value);
+    var maxVal = parseInt(document.getElementById("datamax").value);
 
     // Generate random numbers
     for (var i = 0; i < dataSize; i ++)
-    data.push(Math.round(Math.random() * 100));
+    data.push(Math.round(Math.random() * maxVal));
 
     // Display the data to the page
-    document.getElementById("data").innerHTML = `${data}`;
+    document.getElementById("data").innerHTML = `${[data]}`;
+    document.getElementById("datasorted").innerHTML = `${[data]}`;
 }
 // -------------------------------------------------
 
+// Sorting algorithm: BUBBLE SORT -------------------------------------------------
+function sortBubble() {
+
+    var data = document.getElementById("data").innerHTML;
+
+    document.getElementById("datasorted").innerHTML = `${data}`;
+    alert(data);
+}
+// -------------------------------------------------
